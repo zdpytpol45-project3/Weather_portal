bbd2622d1d8a1ceb4631892a3785b7f9f250bbf9
Robert <rroobb357@gmail.com>
2022-04-24 12:38:08 +0200

    Change datebase on sqlite3

--- WeatherPortal/WeatherPortal/settings.py
+++ WeatherPortal/WeatherPortal/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'WeatherPortal.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': 'mydatabase',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
