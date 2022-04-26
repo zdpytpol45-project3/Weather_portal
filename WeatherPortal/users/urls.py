2240a952732d4bdbb5ba9d974a1163ec0b1d681b
grzegorz jezierski <jezierskigrzegorz1@gmail.com>
2022-04-26 15:19:13 +0200

    Add registration new user

--- WeatherPortal/users/urls.py
+++ WeatherPortal/users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, include
 
-from users.views import dashboard
+from users.views import dashboard, register
 
 urlpatterns = [
     path('dashboard/', dashboard, name='dashboard'),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', register, name='register'),
 
 ]
--- WeatherPortal/users/views.py
+++ WeatherPortal/users/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.urls import reverse
+from users.forms import CustomUserCreationForm
+
+
 # Create your views here.
 
 
@@ -6,4 +11,15 @@ def dashboard(request):
     return render(request, "users/dashboard.html")
 
 
-
+def register(request):
+    if request.method == "GET":
+        return render(
+            request, 'users/register.html',
+            {'form': CustomUserCreationForm}
+        )
+    elif request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect(reverse('dashboard'))
