d3e087329b6a09e7aff25a3fcf5d8f35dcf8e415
Robert <rroobb357@gmail.com>
2022-04-27 23:50:16 +0200

    add delete city function, add more than one city, after add city project check that name already is in our base or city not exist

--- WeatherPortal/weatherapp/urls.py
+++ WeatherPortal/weatherapp/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('your-weather/', views.your_weather),
+    path('your-weather/', views.your_weather, name='weather_in_user_city'),
+    path('your-weather/delete/<city_name_to_delete>', views.delete_user_city, name='delete_user_city')
 ]
