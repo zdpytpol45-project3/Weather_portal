558668024dbd93b99e1e7518042de9a79e277764
Robert <rroobb357@gmail.com>
2022-05-12 19:45:51 +0200

    connect_api_with_user

--- WeatherPortal/weatherapp/models.py
+++ WeatherPortal/weatherapp/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
 
 class City(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=25)
 
     def __str__(self):
